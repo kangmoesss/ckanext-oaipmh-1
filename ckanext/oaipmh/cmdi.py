ae28030e974787cc53e52f790b1f5c9df6c0b95e
kangmoesss <83626340+kangmoesss@users.noreply.github.com>
2022-01-05 12:13:59 +0700

    Update cmdi.py

--- ckanext/oaipmh/cmdi.py
+++ ckanext/oaipmh/cmdi.py
@@ -1,16 +1,33 @@
-import httplib
+import http.client 
 import json
 import logging
-import urllib2
+import urllib.request,urllib.parse,urllib.error
 from lxml import etree
 import oaipmh
-from ckanext.kata.utils import get_package_id_by_pid
+
 from ckanext.oaipmh import importformats
 from ckanext.oaipmh.cmdi_reader import CmdiReader
 from ckanext.oaipmh.harvester import OAIPMHHarvester
+import ckan.plugins.toolkit as toolkit
 
 log = logging.getLogger(__name__)
 
+def get_package_id_by_pid(pid, pid_type):
+    """ Find pid by id and type.
+    :param pid: id of the pid
+    :param pid_type: type of the pid (primary, relation)
+    :return: id of the package
+    """
+    query = select(['key', 'package_id']).where(and_(model.PackageExtra.value == pid, model.PackageExtra.key.like('pids_%_id'),
+                                                     model.PackageExtra.state == 'active'))
+
+    for key, package_id in [('pids_%s_type' % key.split('_')[1], package_id) for key, package_id in Session.execute(query)]:
+        query = select(['package_id']).where(and_(model.PackageExtra.value == pid_type, model.PackageExtra.key == key,
+                                                  model.PackageExtra.state == 'active', model.PackageExtra.package_id == package_id))
+        for package_id, in Session.execute(query):
+            return package_id
+
+    return None
 
 class CMDIHarvester(OAIPMHHarvester):
     md_format = 'cmdi0571'
@@ -25,9 +42,9 @@ class CMDIHarvester(OAIPMHHarvester):
             'description': 'Harvests CMDI dataset'
         }
 
-    def get_schema(self, config, pkg):
-        from ckanext.kata.plugin import KataPlugin
-        return KataPlugin.create_package_schema_oai_cmdi()
+    #def get_schema(self, config, pkg):
+        #from ckanext.kata.plugin import KataPlugin
+        #return KataPlugin.create_package_schema_oai_cmdi()
 
     def on_deleted(self, harvest_object, header):
         """ See :meth:`OAIPMHHarvester.on_deleted`
